6d02f93a0ee6f1e482c74b85ce67b0a04c1a7bc6
sravan <sravan.k.vodnala@gmail.com>
2022-06-23 17:48:34 +0530

    Added: Using Hashtable find frequency of other sentence

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -7,7 +7,7 @@ public class HashTable {
     public static void main(String[] args) {
         System.out.println("Welcome to Hashtable Program");
         Hashtable<String, Integer> hashtable = new Hashtable<>();
-        String input = "To be or not to be";
+        String input = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
         String[] inputArray = input.toLowerCase().split(" ");
         for (String str : inputArray) {
             if (hashtable.containsKey(str)) {
